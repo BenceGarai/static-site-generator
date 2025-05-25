ca2c1500f80c58c3ef5fa6a253730068b8180889
Bence Garai <garaibence@gmail.com>
2025-05-25 12:03:53 -0700

    Add html node and unit tests

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
+from htmlnode import HTMLNode
 from textnode import *
 
 def main():
-    test_text = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev" )
-    print(test_text)
+    test_props = {"href": "https://www.google.com",
+                      "target": "_blank"}
+
+    test_html = HTMLNode("p", "This is a paragraph", None, test_props)
+    print(test_html.props_to_html())
+    print(test_html)
 
 if __name__  == "__main__":
     main()
---
+++ src/test_htmlnode.py
@@ -0,0 +1,22 @@
+import unittest
+
+from htmlnode import *
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        test_props = {"href": "https://www.google.com",
+                      "target": "_blank"}
+        
+        node = HTMLNode("p", "This is a paragraph", None, test_props)
+        self.assertEqual("href=\"https://www.google.com\" target=\"_blank\"", node.props_to_html())
+    
+    
+    def test_repr(self):
+        node = HTMLNode("h1", "This is a header 1", None, None)
+        self.assertEqual("HTMLNode: h1, This is a header 1, None, None", repr(node))
+        
+    
+    def test_none(self):
+        node = HTMLNode("h1", "This is a header 1")
+        node2 = HTMLNode("h1", "This is a header 1", None, None)
+        self.assertEqual(repr(node), repr(node2))
