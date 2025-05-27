e1fcb4f1d7e801d029e22a441624a4c43984ead1
Bence Garai <garaibence@gmail.com>
2025-05-27 08:29:01 -0700

    Add split_nodes_delimiter

---
+++ src/utils.py
@@ -0,0 +1,36 @@
+from textnode import TextType, TextNode
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type: TextType):
+    # node = TextNode("This is text with a `code block` word", TextType.TEXT)
+    # new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+    
+    # becomes
+    
+    # [
+    # TextNode("This is text with a ", TextType.TEXT),
+    # TextNode("code block", TextType.CODE),
+    # TextNode(" word", TextType.TEXT),
+    # ]
+
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type == TextType.TEXT:
+            split_node_text = node.text.split(delimiter)
+            
+            if len(split_node_text) % 2 == 0:
+                raise Exception("Invalid markdown syntax: no closing delimiter")
+            
+            for index, text in enumerate(split_node_text):
+                if len(text) == 0:
+                    continue
+                if index % 2 == 0:
+                    # even index
+                    new_nodes.append(TextNode(text, TextType.TEXT, None))
+                else:
+                    # odd index
+                    new_nodes.append(TextNode(text, text_type, None))
+        else:
+            new_nodes.append(node)
+            
+    return new_nodes
