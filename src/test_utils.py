fbbe52761199cd91f1d537cd69f4d19e90194dfb
Bence Garai <garaibence@gmail.com>
2025-05-27 08:35:17 -0700

    Add tests for utils

--- src/test_utils.py
+++ src/test_utils.py
@@ -31,3 +31,15 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(repr(split_nodes), expected_nodes_repr)
         
             
+    def test_delimiter_at_beginning(self):
+        node = TextNode("`code block` there is a delimiter in the beginning!", TextType.TEXT)
+        split_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected_nodes_repr = "[TextNode(code block, code, None), TextNode( there is a delimiter in the beginning!, text, None)]"
+        self.assertEqual(repr(split_nodes), expected_nodes_repr)
+        
+    
+    def test_delimiter_at_end(self):
+        node = TextNode("There is a delimiter at the end, `code block`", TextType.TEXT)
+        split_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected_nodes_repr = "[TextNode(There is a delimiter at the end, , text, None), TextNode(code block, code, None)]"
+        self.assertEqual(repr(split_nodes), expected_nodes_repr)
