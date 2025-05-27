2eba35673d2ade35de56855e51a396626a000643
Bence Garai <garaibence@gmail.com>
2025-05-27 09:27:31 -0700

    Add test for image and markdown extractions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,19 +3,19 @@ from utils import *
 from textnode import TextType, TextNode
 
 
-class TestUtils(unittest.TestCase):
+class TestSplitNodesDelimiter(unittest.TestCase):
     def test_code_block(self):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         split_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        expected_nodes_repr = "[TextNode(This is text with a , text, None), TextNode(code block, code, None), TextNode( word, text, None)]"
-        self.assertEqual(repr(split_nodes), expected_nodes_repr)
+        matches = "[TextNode(This is text with a , text, None), TextNode(code block, code, None), TextNode( word, text, None)]"
+        self.assertEqual(repr(split_nodes), matches)
         
         
     def test_two_code_blocks(self):
         node = TextNode("This is text with a `code block` word and another `code block` word", TextType.TEXT)
         split_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        expected_nodes_repr = "[TextNode(This is text with a , text, None), TextNode(code block, code, None), TextNode( word and another , text, None), TextNode(code block, code, None), TextNode( word, text, None)]"
-        self.assertEqual(repr(split_nodes), expected_nodes_repr)
+        matches = "[TextNode(This is text with a , text, None), TextNode(code block, code, None), TextNode( word and another , text, None), TextNode(code block, code, None), TextNode( word, text, None)]"
+        self.assertEqual(repr(split_nodes), matches)
 
     
     def test_no_closing_delimiter(self):
@@ -27,19 +27,30 @@ class TestUtils(unittest.TestCase):
     def test_delimiter_not_found(self):
         node = TextNode("This is text with no delimiter in it", TextType.TEXT)
         split_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        expected_nodes_repr = "[TextNode(This is text with no delimiter in it, text, None)]"
-        self.assertEqual(repr(split_nodes), expected_nodes_repr)
+        matches = "[TextNode(This is text with no delimiter in it, text, None)]"
+        self.assertEqual(repr(split_nodes), matches)
         
             
     def test_delimiter_at_beginning(self):
         node = TextNode("`code block` there is a delimiter in the beginning!", TextType.TEXT)
         split_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        expected_nodes_repr = "[TextNode(code block, code, None), TextNode( there is a delimiter in the beginning!, text, None)]"
-        self.assertEqual(repr(split_nodes), expected_nodes_repr)
+        matches = "[TextNode(code block, code, None), TextNode( there is a delimiter in the beginning!, text, None)]"
+        self.assertEqual(repr(split_nodes), matches)
         
     
     def test_delimiter_at_end(self):
         node = TextNode("There is a delimiter at the end, `code block`", TextType.TEXT)
         split_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        expected_nodes_repr = "[TextNode(There is a delimiter at the end, , text, None), TextNode(code block, code, None)]"
-        self.assertEqual(repr(split_nodes), expected_nodes_repr)
+        matches = "[TextNode(There is a delimiter at the end, , text, None), TextNode(code block, code, None)]"
+        self.assertEqual(repr(split_nodes), matches)
+        
+
+class TestExtractMarkdown(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)")
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_links("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)")
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
