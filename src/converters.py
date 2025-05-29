e0a507262d33e00079723ef0a6b81058dfaa8a0b
Bence Garai <garaibence@gmail.com>
2025-05-29 12:15:45 -0700

    Add block to html

--- src/converters.py
+++ src/converters.py
@@ -1,6 +1,9 @@
 from textnode import TextNode, TextType
 from leafnode import LeafNode
 from enum import Enum
+from htmlnode import HTMLNode
+from utils import *
+from parentnode import ParentNode
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -34,13 +37,13 @@ def text_node_to_html_node(text_node: TextNode):
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
-    
-    for index, block in enumerate(blocks):
-        blocks[index] = block.strip()
-        if len(block) == 0:
-            blocks.pop(block)
-        
-    return blocks
+    filtered_blocks = []
+    for block in blocks:
+        if block == "":
+            continue
+        block = block.strip()
+        filtered_blocks.append(block)
+    return filtered_blocks
 
 
 def block_to_block_type(markdown_block: str):
@@ -71,3 +74,95 @@ def block_to_block_type(markdown_block: str):
             i += 1
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        html_node = block_to_html_node(block)
+        children.append(html_node)
+    return ParentNode("div", children, None)
+
+
+def block_to_html_node(block):
+    block_type = block_to_block_type(block)
+    
+    if block_type == BlockType.PARAGRAPH:
+        return paragraph_to_html_node(block)
+    if block_type == BlockType.HEADING:
+        return heading_to_html_node(block)
+    if block_type == BlockType.CODE:
+        return code_to_html_node(block)
+    if block_type == BlockType.ORDERED_LIST:
+        return olist_to_html_node(block)
+    if block_type == BlockType.UNORDERED_LIST:
+        return ulist_to_html_node(block)
+    if block_type == BlockType.QUOTE:
+        return quote_to_html_node(block)
+    raise ValueError("invalid block type")
+
+# take a block of text and convert inline markdown to a list of HTMLNode children 
+def text_to_children(text):
+    result = [text_node_to_html_node(node) for node in text_to_textnodes(text)]
+    return result
+
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
+
+
+def heading_to_html_node(block):
+    level = 0
+    for char in block:
+        if char == "#":
+            level += 1
+        else:
+            break
+    if level + 1 >= len(block):
+        raise ValueError(f"Invalid heading level: {level}")
+    text = block[level + 1 :]
+    children = text_to_children(text)
+    return ParentNode(f"h{level}", children)
+
+
+def code_to_html_node(block):
+    if not block.startswith("```") or not block.endswith("```"):
+        raise ValueError("Invalid codeblock")
+    text = block[4:-3]
+    raw_text_node = TextNode(text, TextType.TEXT)
+    child = text_node_to_html_node(raw_text_node)
+    code = ParentNode("code", [child])
+    return ParentNode("pre", [code])
+    
+def olist_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[3:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ol", html_items)
+
+
+def ulist_to_html_node(block):
+    items = block.split("\n")
+    html_items = []
+    for item in items:
+        text = item[2:]
+        children = text_to_children(text)
+        html_items.append(ParentNode("li", children))
+    return ParentNode("ul", html_items)
+
+
+def quote_to_html_node(block):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("Invalid quote block")
+        new_lines.append(line.lstrip(">").strip())
+    content = " ".join(new_lines)
+    children = text_to_children(content)
+    return ParentNode("blockquote", children)
--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import re
 
 
 def text_to_textnodes(text):
-    nodes = TextNode(text, TextType.TEXT)
+    nodes = [TextNode(text, TextType.TEXT)]
     nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
     nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
     nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
