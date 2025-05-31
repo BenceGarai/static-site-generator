3c7d2f33bd9c61a65594e670e53798ba43376042
Bence Garai <garaibence@gmail.com>
2025-05-30 17:56:03 -0700

    Add basepath to config

--- src/main.py
+++ src/main.py
@@ -1,12 +1,20 @@
+import sys
 from pages import generate_pages_recursive
 from file_handling import copy_static_to_public
 
 def main():
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+        
+    print(f"Basepath: {basepath}")
+    
     print("Copying static files to public directory...")
     copy_static_to_public()
     
     print("Generating content in public directory...")
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
     
 
 if __name__  == "__main__":
--- src/pages.py
+++ src/pages.py
@@ -12,7 +12,7 @@ def extract_title(markdown):
     raise Exception("No h1 found")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     
     with open(from_path, "r") as file:
@@ -26,6 +26,8 @@ def generate_page(from_path, template_path, dest_path):
     
     html_page = template_file.replace("{{ Title }}", title)
     html_page = html_page.replace("{{ Content }}", html_node.to_html())
+    html_page = html_page.replace('href="/', f'href="{basepath}')
+    html_page = html_page.replace('src="/', f'src="{basepath}')
     
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     
@@ -33,12 +35,12 @@ def generate_page(from_path, template_path, dest_path):
         file.write(html_page)
         
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
