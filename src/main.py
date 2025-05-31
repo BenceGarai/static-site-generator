fa7d53a432a969d6f69b1b534dcbbfa53826bb5a
Bence Garai <garaibence@gmail.com>
2025-05-30 17:26:32 -0700

    Add print statments to main

--- src/main.py
+++ src/main.py
@@ -2,7 +2,10 @@ from pages import generate_pages_recursive
 from file_handling import copy_static_to_public
 
 def main():
+    print("Copying static files to public directory...")
     copy_static_to_public()
+    
+    print("Generating content in public directory...")
     generate_pages_recursive("content", "template.html", "public")
     
 
