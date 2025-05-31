2b332f62925582970b9e806850da45e2cff5c6ac
Bence Garai <garaibence@gmail.com>
2025-05-30 17:25:07 -0700

    Add recursive function for page generation

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
-from pages import generate_page
+from pages import generate_pages_recursive
 from file_handling import copy_static_to_public
 
 def main():
-    print("Calling copy function")
     copy_static_to_public()
-    print("Copy complete")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
     
 
 if __name__  == "__main__":
